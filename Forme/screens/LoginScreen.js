945dde211ce5a1666459723938987a3d05cefff0
Office <212219apr@gmail.com>
2024-04-12 01:08:24 +0900

    수정

--- Forme/screens/LoginScreen.js
+++ Forme/screens/LoginScreen.js
@@ -23,18 +23,34 @@ const LoginScreen = () => {
             />
         </View>
         <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>로그인</Text>
-            </TouchableOpacity>
+            <View style={styles.loginContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>로그인</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.linkContainer}>
+                <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+                    <Text style={[styles.linkText, styles.grayText]}>비밀번호 찾기</Text>
+                </TouchableOpacity>
+                { /* <Text style={styles.divider}> | </Text> */ }
+                <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+                    <Text style={[styles.linkText, styles.grayText]}>아이디 찾기</Text>
+                </TouchableOpacity>
+                 { /* <Text style={styles.divider}> | </Text> */ }
+                <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+                    <Text style={[styles.linkText, styles.blueText]}>회원가입</Text>
+                </TouchableOpacity>            
+            </View>
             <TouchableOpacity onPress={() => navigation.navigate('Main')}>
                 <Text style={styles.MainButtonText}>고객센터</Text>
             </TouchableOpacity>
-        </View>
+            </View>
+                
         <Text style={styles.LogoText}>For Me</Text>
     </View>
   )
 }
-
+// 지금 로그인 - 고객센터 - 비밀번호~회원가입 - 로고 이렇게 됨 알아서,, ㅅ정해저ㅏ마로머ㅏ러ㅏ
 export default LoginScreen
 
 const styles = StyleSheet.create({
@@ -48,18 +64,31 @@ const styles = StyleSheet.create({
         width: '50%',
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 30,
-        gap: 15
+        marginTop: 10,
+        gap: 15,
     },
     inputContainer: {
         width: '70%'
+    },
+    loginContainer: {
+        width: '105%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    linkContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
     input: {
         backgroundColor: '#E5E5E5',
         paddingHorizontal: 15,
         paddingVertical: 10,
         borderRadius: 10,
-        marginTop: 5
+        marginTop: 5,
+        borderWidth: 1,
+        borderColor: '#949494'
     },
     button: {
         backgroundColor: '#508BFF',
@@ -70,13 +99,30 @@ const styles = StyleSheet.create({
     },
     buttonText: {
         fontWeight: '900',
-        fontSize: 14
+        fontSize: 14,
+        color: 'white'
+    },
+    linkText:{
+        fontSize: 12,
+        marginRight: 10
+    },
+    grayText: {
+        color: '#545454'
+    },
+    blueText: {
+        color: '#508BFF'
+    },
+    divider: {
+        color: '#545454',
+        fontSize: 12,
+        
     },
     MainButtonText: {
         fontWeight: '900',
         fontSize: 12,
         color: '#868E96',
-        marginTop: 30
+        marginTop: 30,
+        marginBottom: 8
     },
     LogoText: {
         fontWeight: '900',
